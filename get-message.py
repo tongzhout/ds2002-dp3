0ff754618fbcc8d56a7510f9a55333136d5864a7
tongzhout z <tongzhou20030115@gmail.com>
2024-04-22 20:20:28 +0000

    add+modify store message and reassemble phrase

--- get-message.py
+++ get-message.py
@@ -18,41 +18,57 @@ def delete_message(handle):
     except ClientError as e:
         print(e.response['Error']['Message'])
 
+
+
+
 def get_message():
-    try:
-        # Receive message from SQS queue. Each message has two MessageAttributes: order and word
-        # You want to extract these two attributes to reassemble the message
-        response = sqs.receive_message(
-            QueueUrl=url,
-            AttributeNames=[
-                'All'
-            ],
-            MaxNumberOfMessages=1,
-            MessageAttributeNames=[
-                'All'
-            ]
-        )
-        # Check if there is a message in the queue or not
-        if "Messages" in response:
-            # extract the two message attributes you want to use as variables
-            # extract the handle for deletion later
-            order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
-            word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
-            handle = response['Messages'][0]['ReceiptHandle']
+    count = 0;
+    with open('message.txt', 'w') as file:
+        while True:
+            try:
+                response = sqs.receive_message(
+                    QueueUrl=url,
+                    AttributeNames=[
+                        'All'
+                    ],
+                    MaxNumberOfMessages=1,
+                    MessageAttributeNames=[
+                        'All'
+                    ],
+                    VisibilityTimeout = 300
+                )
+                if "Messages" in response:
+                    for msg in response['Messages']:
+                        order = msg['MessageAttributes']['order']['StringValue']
+                        word = msg['MessageAttributes']['word']['StringValue']
+                        handle = msg['ReceiptHandle']
+                        file.write(f"{order},{word},{handle}\n")
+                        count += 1
+                        if count >= 10:
+                            file.close()
+                            return
+                else:
+                    print("No more messages in the queue.")
+                    file.close()
+                    break
+            except ClientError as e:
+                print(e.response['Error']['Message'])
+                break
 
-            # Print the message attributes - this is what you want to work with to reassemble the message
-            print(f"Order: {order}")
-            print(f"Word: {word}")
+def reassemble_phrase():
+    message = []
+    with open('message.txt', 'r') as file:
+        for eachline in file:
+            order, word, handle = eachline.strip().split(',')
+            message.append({"order": int(order), "word": word, "handle": handle})
+    file.close()
+    message.sort(key=lambda x: x['order'])
+    reassembled_phrase = ' '.join([each['word'] for each in message])
+    return reassembled_phrase
 
-        # If there is no message in the queue, print a message and exit    
-        else:
-            print("No message in the queue")
-            exit(1)
-            
-    # Handle any errors that may occur connecting to SQS
-    except ClientError as e:
-        print(e.response['Error']['Message'])
 
 # Trigger the function
 if __name__ == "__main__":
     get_message()
+    reassembled_phrase = reassemble_phrase()
+    print(reassembled_phrase)
